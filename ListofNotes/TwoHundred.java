5ee1d23ad80f8cbdfec5d2379f690d6e179d7e6e
Midura24 <midura2411@gmail.com>
2025-01-24 18:31:36 +0530

    TwoHundred.java

--- ListofNotes/TwoHundred.java
+++ ListofNotes/TwoHundred.java
@@ -2,8 +2,9 @@ package ListofNotes;
 
 import notes.Notes;
 
-public class TwoHundred extends Notes {
-    public TwoHundred(String notes, long notecounts)
+public class TwoHundred extends Notes// create the class and extend the Notes class
+{
+    public TwoHundred(String notes, long notecounts)// constructor to assign the values of note and note count using parent class constructor
     {
         super(notes,notecounts);
     }

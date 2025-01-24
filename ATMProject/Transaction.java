3389801a45b9a00c6dafca95a7d94d9fa2bcf663
Midura24 <midura2411@gmail.com>
2025-01-24 18:26:04 +0530

    Transaction.java

--- ATMProject/Transaction.java
+++ ATMProject/Transaction.java
@@ -1,37 +1,37 @@
 package ATMProject;
 
 public class Transaction {
-    public String performer;
-    public double amount;
-    public String transactionType;
-    public Transaction(String performer, double amount, String transactionType)
+    public String performer;// variable to store the performer type as admin or user
+    public double amount;// variable to store the amount
+    public String transactionType;// variable to store the transaction type list deposit or withdraw
+    public Transaction(String performer, double amount, String transactionType)// constructor to set the values for each variable
     {
         this.setPerformer(performer);
         this.setAmount(amount);
         this.setTransactionType(transactionType);
     }
 
-    public void setPerformer(String performer)
+    public void setPerformer(String performer)// method to set the values to the performer variable performer
     {
         this.performer = performer;
     }
-    public void setTransactionType(String transactionType)
+    public void setTransactionType(String transactionType)// method to set the values to the transaction type variable transaction type
     {
         this.transactionType = transactionType;
     }
-    public void setAmount(double amount)
+    public void setAmount(double amount)// method to set the values to the variable amount
     {
         this.amount = amount;
     }
-    public String getperformer()
+    public String getperformer()//method to return the performer
     {
         return performer;
     }
-    public String getTransactionType()
+    public String getTransactionType()// method to return the transaction type
     {
         return transactionType;
     }
-    public double getAmount()
+    public double getAmount()// method to return the amount
     {
         return amount;
     }

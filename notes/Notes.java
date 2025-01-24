52c32d27403c4c75652cc85ad63c45b7a02b1077
Midura24 <midura2411@gmail.com>
2025-01-24 18:33:06 +0530

    Notes.java

--- notes/Notes.java
+++ notes/Notes.java
@@ -1,27 +1,32 @@
 package notes;
 
-public class Notes {
-    public String note;
-    public long notecount;
-    protected Notes(String note, long notecount)
+public class Notes implements Cloneable//create the class Notes and implements teh cloneable to use the clone
+{
+    public String note;// variable to store the note type
+    public long notecount;// variable to store the note count
+    protected Notes(String note, long notecount)// constructor to assign the values to the note and note count
     {
         this.setNote(note);
         this.setNotecount(notecount);
     }
-    public String getNote()
+    public String getNote()//method to return the note
     {
         return note;
     }
-    public void setNote(String note)
+    public void setNote(String note)// method to set the value of note to the variable note
     {
         this.note = note;
     }
-    public void setNotecount(long notecount)
+    public void setNotecount(long notecount)//method to set the value to the variable note count
     {
         this.notecount = notecount;
     }
-    public long getNotecount()
+    public long getNotecount()//method to return the note count
     {
         return notecount;
+    }
+    public Notes clone() throws CloneNotSupportedException// method to clone the note
+    {
+        return (Notes) super.clone();// Perform a shallow copy using the Object class's clone method.
     }
 }
